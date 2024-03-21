8a3d2b492b6a990bab30ea0370633f725601dfad
Devang Kartik Suryavanshi <93068034+Devangkartik@users.noreply.github.com>
2024-03-22 01:08:32 +0530

    vercel/deploye

--- src/components/NewsIteam.js
+++ src/components/NewsIteam.js
@@ -9,9 +9,10 @@ render() {
         <div className='my-5'>
             <div className="card">
                 <img src={imageUrl?imageUrl:"https://dnd2oi6izkvoi.cloudfront.net/2023/08/26/image/jpeg/KCPzxW1cF99rLfkfm8XtoEHq0ht0sF3Vgjpl6Q4r.jpg"} className="card-img-top" alt="..."/>
-                <div className="card-body">?
+                <div className="card-body">
                   <h5 className="card-title">{title}</h5>
                   <p className="card-text">{description}</p>
+                  <h5 className="card-title">{title}</h5>
                   <p>Published At {new Date(time).toGMTString()} By {author}</p>
                   <a href={newsUrl} target="_blank"  rel="noreferrer" className="btn btn-sm btn-primary">Go somewhere</a>
                 </div>
